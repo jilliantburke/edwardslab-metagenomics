457f468f477829c26543f9609e62533824efea6e
Jillian <35746736+jilliantburke@users.noreply.github.com>
2019-05-16 14:09:00 -0700

    Update focus_parser.py

--- focus_parser.py
+++ focus_parser.py
@@ -1,7 +1,25 @@
+#This file parses the raw output of the focus program
+#It outputs a table with the SRA ID's as headers, the Patric IDs as the first entry in each row (row names)
+'''
+Required inputs are
+1. a directory of results files, files are named so that the ID is first and seperated by a '.'
+2.the files listed in the dictionary must be tab seperated file with one header line and two or three columns. The thrid column is optional. Example format:
+
+Strain  DRR000231_pass.fasta #header line
+Firmicutes bacterium UBA5237 PATRIC|1948037.3   12.026660711310816 #line 1
+Geoglobus sp. UBA232 PATRIC|1915570.3   6.136419201519487 #line 2
+
+3. output file name 
+# Note: do not run from inside the directory of results, the output will be written to the current directory
+'''
+
+
 import argparse
 import os
 import subprocess
 import pandas
+
+
 #open the directory and list all files
 #split them to get just the SRA headers and then use this as the header line
 
@@ -44,50 +62,16 @@ def fix_header(intermediate_file,output_file):
 				
 				else:
 					outf.write(line)
-#add each patric ID to a set so that there is a full set of IDs, use this as the column names
-
-#each count is stored as an entry an entry in a dictionary, with IDs as the key
-
-
-#create a table with the SRA ID's as headers, the Patric IDs as the first entry in each row (row names)
-#fill with zeros
-
-#iterate through each dictionary entry, and fill in the counts in the table created above
 
 #write to file
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Parse a directory of focus files and generate a single tsv file")
     parser.add_argument('-d', help='directory of focus output files, and nothing else. Files should start with the genome ID', required=True)
     parser.add_argument('-o', help='output filename', required=True)
-#    parser.add_argument('-v', help='verbose output', action="store_true")
     args = parser.parse_args()
-
-#    test_data_path='C:\\Users\JTB\Documents\edwards_lab\projects\mgenomes_clusters\\test_data\\'
-#    outf='C:\\Users\JTB\Documents\edwards_lab\projects\mgenomes_clusters\\test_data\\focus_test_data_output.tsv'
     intm_file='focus_file_intermediate.tsv'
     read_counts= file_parse(args.d)
     #print(sra_ids,patric_ids, read_counts)
     file_writer(intm_file,read_counts)
     fix_header(intm_file,args.o)
     subprocess.call(["rm", intm_file])
-'''
-to run from the command line:
-1. provide the name of directory contain only results files
-2. the results files must be named so that the SRA id is first and seperated by a '.'
-3.the results files must be tab seperated file with on header line and two or three columns. The thrid column is optional. Example format:
-Strain  DRR000231_pass.fasta 
-Firmicutes bacterium UBA5237 PATRIC|1948037.3   12.026660711310816
-Geoglobus sp. UBA232 PATRIC|1915570.3   6.136419201519487
-Klebsiella pneumoniae PATRIC|573.16228  0.4643423056034224
-Klebsiella pneumoniae PATRIC|573.16232  0.494672270710047
-Lachnospiraceae bacterium UBA2942 PATRIC|1952019.3      5.301575914410268
-Lactobacillus backii PATRIC|375175.76   0.14775219399495737
-Lactobacillus delbrueckii PATRIC|767455.7       0.2672115673522007
-Lactobacillus salivarius PATRIC|1624.129        0.002988559684395548
-Marine Group II euryarchaeote PATRIC|2163009.67 3.19890019358266
-.
-.
-.
-4. provde the name of the output file 
-#5. Note: do not run from inside the directory of results, the output will be written to the current directory
-'''
